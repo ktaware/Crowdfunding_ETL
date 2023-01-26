5538a7a3d533e6c4eab2f001c93bc172e001185a
komal <mail.to.komalv@gmail.com>
2023-01-26 17:23:48 -0500

    ETL

--- crowdfunding_db_schema.sql.sql
+++ crowdfunding_db_schema.sql.sql
@@ -5,34 +5,34 @@
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" string   NOT NULL,
-    "description" string   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR    NOT NULL,
     "goal" float   NOT NULL,
     "pledged" float   NOT NULL,
-    "outcome" string   NOT NULL,
+    "outcome" VARCHAR    NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" string   NOT NULL,
-    "currency" string   NOT NULL,
+    "country" VARCHAR    NOT NULL,
+    "currency" VARCHAR    NOT NULL,
     "launch_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" string   NOT NULL,
-    "subcategory_id" string   NOT NULL,
+    "category_id" VARCHAR    NOT NULL,
+    "subcategory_id" VARCHAR    NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" string   NOT NULL,
-    "category" string   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR    NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" string   NOT NULL,
-    "subcategory" string   NOT NULL,
+    "subcategory_id" VARCHAR    NOT NULL,
+    "subcategory" VARCHAR    NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -40,9 +40,9 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "email" string   NOT NULL,
+    "first_name" VARCHAR    NOT NULL,
+    "last_name" VARCHAR    NOT NULL,
+    "email" VARCHAR    NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
